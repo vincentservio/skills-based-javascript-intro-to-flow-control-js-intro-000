a35f75d334eb8ddf1dc1c48336bd63d9ecc5848b
vincent servio <vincentservio@gmail.com>
2019-08-13 04:33:10 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,40 @@
 function basicTeenager(age) {
+   if (age >=13 && age <=19) {
+   return "You are a teenager!";
 
 }
+ 
+ }
 
 function teenager(age) {
-
-}
+  if (age >=13 && age<=19){
+  return "You are a teenager!";
+  }
+ else {
+   return "You are not a teenager";}
+ }
 
 function ageChecker(age) {
-
+  if (age<13){
+return "You are a kid"
+}
+  if (age>19){
+    return "You are a grownup"
+  }
+  if (age >=13 && age<=19){
+  return "You are a teenager!";}
 }
 
 function ternaryTeenager(age) {
-
+if (age >=13 && age<=19){
+  return  "You are a teenager" ;
 }
+else {
+  return "You are not a teenager";
+}}
 
 function switchAge(age) {
-
-}
+  switchAge (age >=13 && age<=19);
+  case {
+     
+}}
