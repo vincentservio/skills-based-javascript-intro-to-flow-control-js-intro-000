8d12f0a2afd4a2ffba9cb217ab59875891749a59
vincent servio <vincentservio@gmail.com>
2019-08-13 05:41:11 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -38,7 +38,9 @@ function switchAge(age) {
   switch (age){
   case (age >13 && age<19) :
   console.log ("You are a teenager");
-  
+  break; 
+  default:
+  console.log ("You have an age");
   
      
 }}
