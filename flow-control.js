a57770e919eebec197fe7cb05a86e622bed866d5
vincent servio <vincentservio@gmail.com>
2019-08-13 05:52:27 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -37,10 +37,10 @@ function ternaryTeenager(age) {
 function switchAge(age) {
   switch(age){
   case (age >=13 && age <=19):
-  return("You are a teenager")
+  return "You are a teenager"
   break; 
   default:
-  return("You have an age");
+  return "You have an age"
   
      
 }}
