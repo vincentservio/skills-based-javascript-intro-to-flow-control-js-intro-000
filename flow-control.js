4a220df0531ff0e73641e4abaa6b9bb6d9e983b4
vincent servio <vincentservio@gmail.com>
2019-08-13 05:03:03 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,8 +29,8 @@ function ageChecker(age) {
 
 function ternaryTeenager(age) {
   (age >=13 && age<=19)?
-  return : "You are a teenager"
-  
+  return  "You are a teenager":
+  return "You are not a teenager"
 }
 
 
